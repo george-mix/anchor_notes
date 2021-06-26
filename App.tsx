f8764d26374361d2d8021c58eda20322cfffffeb
George <gosha.gosha9696@gmail.com>
2021-06-26 19:31:56 +0300

    setuo jest

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { Text } from "react-native";
 
-const App = () => {
-  return <Text>Hello</Text>;
+import MainNavigator from "./src/navigation/MainNavigator";
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { NavigationContainer } from "@react-navigation/native";
 
 import App from "../App";
+import MainNavigator from "../src/navigation/MainNavigator";
 
 describe("App", () => {
-  it("renders without crashing", () => {
-    shallow(<App />);
+  it("renders navigation container", () => {
+    const wrapper = shallow(<App />);
+    const container = wrapper.find(NavigationContainer);
+
+    expect(container).toHaveLength(1);
+  });
+
+  it("renders navigation", () => {
+    const wrapper = shallow(<App />);
+    const navigator = wrapper.find(MainNavigator);
+
+    expect(navigator).toHaveLength(1);
   });
 });
